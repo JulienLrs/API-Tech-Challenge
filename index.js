a1d5ae8fc174322bab42ae2f5012ade7a7609083
Julien Larrouse <julien.larrouse@gmail.com>
2022-10-12 13:31:35 +0200

    6th update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express= require('express');
+const cors = require('cors');
 require("./db/config");
 const User = require('./db/User');
-const app= express();
+const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 
 app.post("/register", async (req, res) => {
